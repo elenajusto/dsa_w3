e18ed02fe629325e028eeb75cc6ed5401aa69961
Elena Justo <intern@arlula.com>
2026-02-10 10:46:27 +1100

    edit debug log

--- arrays/bubble_sort/main.go
+++ arrays/bubble_sort/main.go
@@ -9,24 +9,18 @@ func main() {
 
 	for index := range len(my_array) {
 
-		log.Println("index being checked: ", index)
-
 		for sub_index := range len(my_array) - index - 1 {
-
-			log.Print("sub_index being checked: ", sub_index, " with value: ", my_array[sub_index])
 
 			// Establish the values
 			check_1 := &my_array[sub_index]
 			check_2 := &my_array[sub_index+1]
 
 			// Check the values
-			log.Println("comparing sub index ", sub_index, " with value ", *check_1, " with sub index ", sub_index+1, " with value ", *check_2)
 			if *check_1 > *check_2 {
 				// Swap if conditon met
-				log.Print("preforming swap")
+				log.Print("bubbling ", *check_1)
 				*check_1, *check_2 = *check_2, *check_1
 			}
-
 			log.Println("array after iteration:", my_array)
 		}
 	}
